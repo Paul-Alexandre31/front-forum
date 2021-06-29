5067f4944a093abe30efc02e84270cf89ce390c8
El Roma <r.huc@orange.fr>
2021-06-29 10:28:23 +0200

    post

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('views', `${__dirname}/src`);
 app.set('view engine', 'twig');
 app.use('/static', express.static(__dirname + '/public'));
 const axiosIntance = axios.create({
-    baseURL:"localhost:1000/api"
+    baseURL: "localhost:1000"
 })
 
 // This section is optional and used to configure twig.
@@ -16,40 +16,36 @@ app.set("twig options", {
     strict_variables: false
 });
 
-app.get('/', function (req, res) {
-
-    res.render('page/home',{
-
-    } );
+app.get('/', async function (req, res) {
+    const messages = await axiosIntance.get("message")
+    res.render('page/home', {messages});
 });
-app.get('/rhum', function (req, res) {
-    res.render('page/rhum' );
+app.get('/rhum', async function (req, res) {
+    res.render('page/rhum');
 });
 app.get('/whisky', function (req, res) {
-    res.render('page/whisky' );
+    res.render('page/whisky');
 });
 app.get('/armagnac', function (req, res) {
-    res.render('page/armagnac' );
+    res.render('page/armagnac');
 });
 app.get('/register', function (req, res) {
-    res.render('page/register' );
+    res.render('page/register');
 });
 app.get('/connect', function (req, res) {
-    res.render('page/connect' );
+    res.render('page/connect');
 });
 app.get('/static', function (req, res) {
-    res.render('img' );
+    res.render('img');
 });
 app.get('/write_message', function (req, res) {
     res.render('page/write_message');
 });
 
-app.get('/post',async function (req, res) {
-    //création variable que l'on integre a coté de page/poste apres la virgule, enssuite on va dans le fichier voulu soit post, une fois dans post on ajoute le variable avec la boucle for
-   let post_info = await axiosIntance.get("page/write_message") //mettre fin url donc /post
-    res.render('page/post_info',post_info);
+app.get('/post', async function (req, res) {
+    let post_info = await axiosIntance.get("page/write_message")
+    res.render('page/post_info', post_info);
 });
-
 
 
 console.log('server run ...')
